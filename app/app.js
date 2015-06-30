8a143fe2c926ce3eaaab5770f5392b7daf402d17
Burak Çalık <burak@macellan.net>
2015-06-30 17:49:03 +0300

    First version.

--- app/app.js
+++ app/app.js
@@ -1,160 +1,30 @@
 var app = require('app');  // Module to control application life.
 var Menu = require('menu');
 var Tray = require('tray');
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
-var mainWindow = null;
+var spawn = require('child_process').spawn;
+var child = null;
 var appIcon = null;
-var win = null;
 
-var template = [
-  {
-    label: 'Whatsapp',
-    submenu: [
-      {
-        label: 'About Whatsapp',
-        selector: 'orderFrontStandardAboutPanel:'
-      },
-      { type: 'separator' },
-      {
-        label: 'Hide Whatsapp',
-        accelerator: 'Command+H',
-        selector: 'hide:'
-      },
-      {
-        label: 'Hide Others',
-        accelerator: 'Command+Shift+H',
-        selector: 'hideOtherApplications:'
-      },
-      {
-        label: 'Show All',
-        selector: 'unhideAllApplications:'
-      },
-      { type: 'separator' },
-      {
-        label: 'Quit',
-        accelerator: 'Command+Q',
-        selector: 'terminate:'
-      },
-    ]
-  },
-  {
-    label: 'Edit',
-    submenu: [
-      {
-        label: 'Undo',
-        accelerator: 'Command+Z',
-        selector: 'undo:'
-      },
-      {
-        label: 'Redo',
-        accelerator: 'Shift+Command+Z',
-        selector: 'redo:'
-      },
-      { type: 'separator' },
-      {
-        label: 'Cut',
-        accelerator: 'Command+X',
-        selector: 'cut:'
-      },
-      {
-        label: 'Copy',
-        accelerator: 'Command+C',
-        selector: 'copy:'
-      },
-      {
-        label: 'Paste',
-        accelerator: 'Command+V',
-        selector: 'paste:'
-      },
-      {
-        label: 'Select All',
-        accelerator: 'Command+A',
-        selector: 'selectAll:'
-      }
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
-      {
-        label: 'Reload',
-        accelerator: 'Command+R',
-        click: function() { win.reload(); }
-      },
-      {
-        label: 'Toggle DevTools',
-        accelerator: 'Alt+Command+I',
-        click: function() { win.toggleDevTools(); }
-      },
-    ]
-  },
-  {
-    label: 'Window',
-    submenu: [
-      {
-        label: 'Minimize',
-        accelerator: 'Command+M',
-        selector: 'performMiniaturize:'
-      },
-      {
-        label: 'Close',
-        accelerator: 'Command+W',
-        selector: 'hide:'
-      },
-      { type: 'separator' },
-      {
-        label: 'Bring All to Front',
-        selector: 'arrangeInFront:'
-      }
-    ]
+function toggle (e) {
+  if (e.checked) {
+    child = spawn('caffeinate');
+    appIcon.setImage(__dirname+'/assets/onTemplate.png');
   }
-];
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  app.quit();
-});
+  else {
+    child.kill();
+    appIcon.setImage(__dirname+'/assets/offTemplate.png');
+  }
+}
 
 // Initialization.
 app.on('ready', function() {
-  // Configuration
-  appIcon = new Tray(__dirname+'/assets/trayTemplate.png');
-  appIcon.on("clicked", function(){
-    win.show();
-  });
-  appIcon.setToolTip('Whatsapp');
-
-  var menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
-
-  // Create the browser window.
-  win = new BrowserWindow({
-    "width": 1000,
-    "height": 720,
-    "type": "toolbar",
-    "title": "Whatsapp",
-    "node-integration": false,
-  });
-
-  // Load the app page
-  win.loadUrl('https://web.whatsapp.com', {
-    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.52 Safari/537.36'
-  });
-
-  // Open links externally
-  win.webContents.on("new-window", function(event, url, frameName, disposition){
-    require('shell').openExternal(url)
-    event.preventDefault();
-  });
-
-  // Open the devtools.
-  // win.openDevTools();
-
-  // Emitted when the window is closed.
-  win.on('closed', function() {
-    win = null;
-  });
-
-  // app.dock.hide();
-  win.show();
+  appIcon = new Tray(__dirname+'/assets/onTemplate.png');
+  var contextMenu = Menu.buildFromTemplate([
+    { label: 'Active', type: 'checkbox', checked: true, click: toggle },
+    { label: 'Quit', click: app.quit },
+  ]);
+  appIcon.setToolTip('Manapot');
+  appIcon.setContextMenu(contextMenu);
+  
+  child = spawn('caffeinate');
 });
--- readme.md
+++ readme.md
@@ -1,28 +1,14 @@
-# Whatsapp Desktop
+# Manapot
 
-Whatsapp desktop client for OSX, based on the official whatsapp web app. Build with [Electron](http://electron.atom.io/).  
-
-This is **NOT** an official product. This project does not attempt to reverse engineer the WhatsApp API or attempt to reimplement any part of the WhatsApp client. Any communication between the user and WhatsApp servers is handled by official WhatsApp Web itself; this is just a native wrapper for WhatsApp Web, like a browser.
-
-## Features
-
-* Native notifications.  
-* System tray icon.  
-* Open links in browser.  
-
-**Planned features:**  
-
-* Badge with the number of notifications in the dock/taskbar.  
-* Auto-launch on OS startup.  
-* Windows and Linux builds.  
+Manapot is a toolbar app, that prevent your Mac from sleeping. It simply runs the "caffeinate" shell command which is an OSX feature. Manapot is build with [Electron](http://electron.atom.io/).  
 
 ## Installation
 
-Download and run the Whatsapp.app file from the [latest release](https://github.com/bcalik/Whatsapp-Desktop/releases).
+Download and run the Manapot.app file from the [latest release](https://github.com/bcalik/Manapot/releases).
 
 ## Contributions
 
-Contributions are welcome! For feature requests and bug reports please submit an [issue](https://github.com/bcalik/whatsapp-desktop/issues).
+Contributions are welcome! For feature requests and bug reports please submit an [issue](https://github.com/bcalik/Manapot/issues).
 
 ## Build
 
@@ -30,3 +16,7 @@ To build from the source, run the following commands:
 
 `npm install`  
 `sudo npm run build`  
+
+--
+
+> Made with :heart: at [Macellan](http://macellan.net)
