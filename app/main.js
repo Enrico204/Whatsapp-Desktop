b24aeb6265a732b3768ef862b8be505b722f23f7
rephole <nctozal@gmail.com>
2015-09-11 17:23:46 +0300

    Code refactoring is completed.

--- app/main.js
+++ app/main.js
@@ -7,79 +7,97 @@
     var fileSystem = require('fs');
     var BrowserWindow = require('browser-window');
 
-    module.exports =
-        global.whatsApp = {
-            init: function() {
-                this.createMenu();
-                this.createTray();
+    global.onlyOSX = function(callback) {
+        if (process.platform === 'darwin') {
+            return Function.bind.apply(callback, this, [].slice.call(arguments, 0));
+        }
+        return function() {};
 
-                this.clearCache();
-                this.openWindow();
+    };
 
-                app.on('window-all-closed', onlyWin(function() {
-                    app.quit()
-                }));
-            },
-            createMenu: function() {
-                this.menu =
-                    appMenu.buildFromTemplate(require('./menu'));
-                    appMenu.setApplicationMenu(this.menu);
-            },
-            createTray: function() {
-                this.tray = new appTray(__dirname + '/img/trayTemplate.png');
+    global.onlyWin = function(callback) {
+        if (process.platform === 'win') {
+            return Function.bind.apply(callback, this, [].slice.call(arguments, 0));
+        }
+        return function() {};
+    };
 
-                this.tray.on('clicked', function() {
-                    this.window.show();
-                }.bind(this));
+    global.whatsApp = {
+        init: function() {
+            this.createMenu();
+            this.createTray();
 
-                this.tray.setToolTip('WhatsApp Desktop');
-            },
-            clearCache: function() {
-                try{
-                    fileSystem.unlinkSync(app.getPath('appData') + '/Application Cache/Index');
-                }catch(e){}
-            },
-            openWindow: function() {
-                this.window = new BrowserWindow(
-                    {
-                        'width': 1280,
-                        'height': 720,
-                        'title': '',
-                        'node-integration': false
-                    }
-                );
-                this.window.loadUrl('https://web.whatsapp.com', {
-                    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.52 Safari/537.36'
-                });
+            this.clearCache();
+            this.openWindow();
+
+            app.on('window-all-closed', onlyWin(function() {
+                app.quit()
+            }));
+        },
+        createMenu: function() {
+            this.menu =
+                appMenu.buildFromTemplate(require('./menu'));
+                appMenu.setApplicationMenu(this.menu);
+        },
+        createTray: function() {
+            this.tray = new appTray(__dirname + '/assets/img/trayTemplate.png');
+
+            this.tray.on('clicked', function() {
                 this.window.show();
+            }.bind(this));
 
-                this.window.on('page-title-updated', onlyOSX(function(event, title) {
-                    var count = title.match(/\((\d+)\)/);
+            this.tray.setToolTip('WhatsApp Desktop');
+        },
+        clearCache: function() {
+            try{
+                fileSystem.unlinkSync(app.getPath('appData') + '/Application Cache/Index');
+            }catch(e){}
+        },
+        openWindow: function() {
+            this.window = new BrowserWindow(
+                {
+                    'width': 1000,
+                    'height': 720,
+                    'title': '',
+                    'node-integration': false
+                }
+            );
+            this.window.loadUrl('https://web.whatsapp.com', {
+                userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.52 Safari/537.36'
+            });
+            this.window.show();
 
-                    if (count !== null) {
-                        app.dock.setBadge(count);
+            this.window.on('page-title-updated', onlyOSX(function(event, title) {
+                var count = title.match(/\((\d+)\)/);
 
-                        if (count > 0) {
-                            app.dock.bounce('informational');
-                        }
+                if (count !== null) {
+                    app.dock.setBadge(count);
+
+                    if (count > 0) {
+                        app.dock.bounce('informational');
                     }
-                }));
+                }
+            }));
 
-                this.window.webContents.on("new-window", function(e, url){
-                    require('shell').openExternal(url);
+            this.window.webContents.on("new-window", function(e, url){
+                require('shell').openExternal(url);
+                e.preventDefault();
+            });
+
+            this.window.on('close', function(e){
+                if (!this.window.forceClose && process.platform === 'darwin') {
                     e.preventDefault();
-                });
+                    this.window.hide();
+                }
+            }.bind(this));
 
-                this.window.on('close', function(e){
-                    if (!this.window.forceClose && process.platform === 'darwin') {
-                        e.preventDefault();
-                        this.window.hide();
-                    }
-                }.bind(this));
+            app.on('before-quit', function() {
+                this.window && (this.window.forceClose = true);
+            }.bind(this));
+        }
+    };
 
-                app.on('before-quit', function() {
-                    this.window && (this.window.forceClose = true);
-                }.bind(this));
-            }
-        };
+    app.on('ready', function() {
+        whatsApp.init();
+    });
 })(this);
