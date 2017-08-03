e0b8c5bcc47579dde44dea2dcdd5acc3a041771c
Enrico204 <enrico204@gmail.com>
2017-08-03 10:01:23 +0200

    Added internal script

---
+++ .prepare-release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+node -v 2>&1 > /dev/null
+if [ $? -ne 0 ]; then
+    echo "Missing nodejs executable"
+    exit 1
+fi
+
+editor package.json
+editor app/package.json
+editor debian/changelog
+editor whatsapp-desktop.spec
+
+npm install
+pushd app
+npm install
+popd
